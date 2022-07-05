800322c098c5609383b1d2a69bf00eb4518aa92a
Megape4enka <mega.pe4enka@gmail.com>
2022-07-05 23:33:00 +0300

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) {
+      let positiveNumber = n * -1
+      let str = positiveNumber + ''
+      let arr = Math.abs(str.split('').reverse().join(''))
+      return arr
+  } else {
+      let str = n + ''
+      let arr = Math.abs(str.split('').reverse().join(''))
+      return arr
+  }
 }
